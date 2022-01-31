e768f20019029d4f9ad93af50b522fa20a0b0656
agboola <agboolanathaniel@yahoo.com>
2022-01-31 23:21:34 +0100

    fix: AC button does not work
    there is no logic for when the buttonName is AC. So I created one by adding an If condition which returns a Calculator data object containing total: null, next: null, operation: null. This thereby resets everything

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -13,6 +13,14 @@ import isNumber from "./isNumber";
  *   operation:String  +, -, etc.
  */
 export default function calculate(obj, buttonName) {
+  if (buttonName === "AC") {
+    return {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
   if (isNumber(buttonName)) {
     if (buttonName === "0" && obj.next === "0") {
       return {};
